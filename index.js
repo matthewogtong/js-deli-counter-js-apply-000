3e01df9725b8f11126d359d02c11dab2520f43e1
Matthew Ogtong <ogtongm@gmail.com>
2020-07-31 14:41:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name)
-  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
-}
-
-function nowServing(katzDeliLine) {
-  if (katzDeliLine.length > 0) {
-    return `Currently serving ${katzDeliLine.shift()}.`
-  } else {
-    return "There is nobody waiting to be served!";
-  }
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line. `
 }
 
 function currentLine(katzDeliLine) {
