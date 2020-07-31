14541ae6e5acdd4af0a7a0eb54fe2b091cd57132
Matthew Ogtong <ogtongm@gmail.com>
2020-07-31 14:27:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function takeANumber(katzDeliLine, name) {
-  katzDeliLine.push(name);
-  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
+  katzDeliLine.push(name)
+  return `Welcome, Ada. You are number ${katzDeliLine.length}`
 }
 
 function nowServing(katzDeliLine) {
