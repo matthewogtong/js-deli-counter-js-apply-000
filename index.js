bcb3d3cf60ef3de0768041fd727f876483d30947
Matthew Ogtong <ogtongm@gmail.com>
2020-07-31 14:57:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function currentLine(katzDeliLine) {
     for (let i = 0; i < katzDeliLine.length; i++) {
       output += (i + 1) + ". " + katzDeliLine[i] + ", ";
     }
-    output = output.slice(0, output.length - 2);
+
     return "The line is currently: " + output;
   } else {
     return "The line is currently empty.";
